619fc3451d95030b245f9c2e98c3f4ef44a58cbc
M-Mubashir674 <mmubashir.bcss19@iba-suk.edu.pk>
2022-02-06 15:20:50 +0500

    addingSignInForm

---
+++ src/Components/App.js
@@ -0,0 +1,11 @@
+import SignInForm from "./SignInForm";
+
+const App = () => {
+  return (
+    <div className="App">
+      <SignInForm></SignInForm>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './Components/App.js';
+import 'antd/dist/antd.min.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
