358df5380fc97ba3a38ee93439c5597b1bb5c91a
M-Mubashir674 <mmubashir.bcss19@iba-suk.edu.pk>
2022-03-06 12:17:06 +0500

    back to localhost

--- src/http-common.js
+++ src/http-common.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export default axios.create({
-    baseURL:"http://192.168.10.16:8080",
+    baseURL:"http://localhost:8080",
     header:{
         "Content-type":"application/json"
     }
